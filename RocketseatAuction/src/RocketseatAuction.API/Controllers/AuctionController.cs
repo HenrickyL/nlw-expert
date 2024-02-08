c58ab6740beacb461205a6975c2e2b2f8da5cc77
HenrickyL <HenrickyL1@gmail.com>
2024-02-08 10:46:11 -0300

    remove un used & adjust namespace format

--- RocketseatAuction/src/RocketseatAuction.API/Controllers/AuctionController.cs
+++ RocketseatAuction/src/RocketseatAuction.API/Controllers/AuctionController.cs
@@ -1,11 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
-namespace RocketseatAuction.API.Controllers
+namespace RocketseatAuction.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuctionController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AuctionController : ControllerBase
-    {
-    }
+
 }
